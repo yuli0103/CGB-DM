e9dc65460deb3b92a8f7d8359242d19d2a706230
yuli <101625845+yuli0103@users.noreply.github.com>
2024-07-19 14:01:10 +0800

    second commit in pycharm

--- data_process/util.py
+++ data_process/util.py
@@ -116,7 +116,6 @@ def update_ema(ema_model, model, decay=0.9999):
     model_params = OrderedDict(model.named_parameters())
 
     for name, param in model_params.items():
-        # TODO: Consider applying only to params that require_grad to avoid small numerical changes of pos_embed
         ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
 
 def get_parameter_number(model):
